4de69785b84cd8b78f7c7d96f78893d92280a491
santiaago <santiago.arias@red-gate.com>
2016-12-13 16:17:37 +0000

    create csharp client

--- csharp.client/Program.cs
+++ csharp.client/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
+using Reverse;
 
 namespace csharp.client
 {
-    class Program
     {
         static void Main(string[] args)
         {
+            var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new ReverseService.ReverseServiceClient(channel);
+
+            var reply = client.ReverseString(new ReverseRequest
+            {
+                Data = "Hello, World"
+            });
+
+            Console.WriteLine("Got: " + reply.Reversed);
+
+            channel.ShutdownAsync().Wait();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
